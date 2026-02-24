1cd8b0904af977ce69a0fd7261780333e7ee783a
Tony Jordan <tjgjsj@me.com>
2026-02-24 11:51:52 -0500

    Stop tracking Salesforce cache files and update settings

--- .sfdx/typings/lwc/apex/FlexTemplateController.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "@salesforce/apex/FlexTemplateController.getFlexTemplateResponse" {
-  export default function getFlexTemplateResponse(param: {AccName: any, rating: any}): Promise<any>;
-}
